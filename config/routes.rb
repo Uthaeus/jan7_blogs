2e6182fc4693665e87ee762d9b391257464effea
Uthaeus <Uth@github.com>
2018-01-07 11:25:31 -0700

    Implemented testing to reach homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   
   resources :blogs
+
+  root to: 'static#homepage'
 end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # describe 'creation' do
+  #   it 'can be created' do
+      
+  #   end
+  # end
 end
