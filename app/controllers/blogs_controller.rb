a0eff221f7d39244dc9adc82d243963dd19f4ace
Uthaeus <Uth@github.com>
2018-01-07 12:51:35 -0700

    Created New and Show for Blogs using testing

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,39 @@
 class BlogsController < ApplicationController
+  before_action :set_post, only: [:show]
 
   def index
+    @blog = Blog.all
   end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+
+    if @blog.save
+      redirect_to @blog, notice: 'Your blog was successfully created'
+
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+
+
+  private
+
+    def blog_params
+      params.require(:blog).permit(:title, :date, :body)
+    end
+
+    def set_post
+      @blog = Blog.find(params[:id])
+    end
 
 end
--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -2,14 +2,34 @@ require 'rails_helper'
 
 describe 'navigate' do
   describe 'index' do
-    it 'can be reached successfully' do
+    before do
       visit blogs_path
+    end
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Blogs' do
-      visit blogs_path
       expect(page).to have_content(/Blogs/)
+    end
+  end
+
+  describe 'creation' do
+    before do
+      visit new_blog_path
+    end
+    
+    it 'nas a new form that can be reached' do
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do
+      fill_in 'blog[title]', with: 'A title'
+      fill_in 'blog[date]', with: Date.today
+      fill_in 'blog[body]', with: "Some content"
+      click_on 'Save'
+
+      expect(page).to have_content("Some content")
     end
   end
 end
